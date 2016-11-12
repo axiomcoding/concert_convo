8c8b2cc8361a7f85c777acdf97262931d7b4d6af
github username <github email address>
2016-11-11 18:09:53 -0700

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# concert_convos_pt2
+Let's use conditionals to determine how loud to speak at a concert. Follow the instructions in `concert_convos_p2.rb` to pass each RSpec test.
---
+++ concert_convo.rb
@@ -0,0 +1,14 @@
+# define a method, concert_convo, that takes in 1 argument, phrase
+# the concert is loud, so your friend can only hear you if you shout (must be in all caps)
+# return the expected strings depending on if your friend can hear you or not
+
+
+
+
+
+
+
+
+
+
+
